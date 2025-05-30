84aaf6c6555fa41c475aca989a21132345ffa2d6
esther-vitoria <esther.vitoria21@outlook.com>
2025-05-30 19:34:32 -0300

    Exercício 4 - Quantos anos tem

--- go-bases/Estrutura de controle/Exercício 4 - Quantos anos tem/main.go
+++ go-bases/Estrutura de controle/Exercício 4 - Quantos anos tem/main.go
@@ -1,1 +1,26 @@
 package main
+
+import "fmt"
+
+func main() {
+	var employees = map[string]int{"Benjamin": 20, "Nahuel": 26, "Brenda": 19, "Darío": 44, "Pedro": 30}
+
+	fmt.Println("Map inicial: ", employees)
+
+	fmt.Println("Imprimindo a idade de Benjamin: ", employees["Benjamin"]) //Imprime a idade de Benjamin
+
+	qtyFuncionario := 0
+	for _, element := range employees { // for que verifica a idade dos funcionários e imprime quantos tem idade superior de 21 anos
+
+		if element > 21 {
+			qtyFuncionario++
+		}
+	}
+	fmt.Println("Quantidade de funcionários acima de 21 anos: ", qtyFuncionario)
+
+	employees["Federico"] = 25 // Adicionando Federico de 25 anos a lista
+	fmt.Println("Adicionando Federico a lista ", employees)
+
+	delete(employees, "Pedro") // Deletando Pedro da lista
+	fmt.Println("Deletando Pedro da lista: ", employees)
+}
