582a2e8d41ba1213e05d933496146da9cd34480f
esther-vitoria <esther.vitoria21@outlook.com>
2025-05-30 18:48:22 -0300

    Exercício 3 - Qual o mês

--- go-bases/Estrutura de controle/Exercício 3 - Qual mês/main.go
+++ go-bases/Estrutura de controle/Exercício 3 - Qual mês/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// Para esse exercício foi feito um array que contém todos os meses do ano, a solução recebe um valor com o numero do mês, faz o decremento desse valor para que
+// seja acessado o índice correto dentro do array, também é feito uma checkagem para verificar se o número de entrada é um mês válido
 func main() {
 	numeroMes := 7
 	meses := []string{"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"}
