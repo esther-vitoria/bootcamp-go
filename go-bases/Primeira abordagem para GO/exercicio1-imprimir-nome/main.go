857f64ac1e7cd65a01fd492f190a89374350fdfd
esther-vitoria <esther.vitoria21@outlook.com>
2025-05-30 16:19:51 -0300

    Organizando pastas, primeiro conteúdo - Abordagens para GO

---
+++ go-bases/Primeira abordagem para GO/exercicio1-imprimir-nome/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var name string
+var endereco string
+
+func main() {
+	name = "Esther Vitoria"
+	endereco = "Rua União"
+	fmt.Println("Nome: ", name)
+	fmt.Println("Endereço: ", endereco)
+}
