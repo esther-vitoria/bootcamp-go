c4115fc9b215e17c45e381a9a78651911e3535a2
esther-vitoria <esther.vitoria21@outlook.com>
2025-05-30 16:47:48 -0300

    Exercicio 1 - Letras em uma palavra

--- go-bases/Estrutura de controle/Exercicio 1 - Letras em uma palavra/main.go
+++ go-bases/Estrutura de controle/Exercicio 1 - Letras em uma palavra/main.go
@@ -8,6 +8,6 @@ func main() {
 	fmt.Println("Número de letras: ", len(palavra)) // Aqui estou imprimindo o quantas letras a palavra tem utilizando a função len(variavel)
 
 	for _, letra := range palavra { //Nesse for estou usando for seguido do underscore para percorrer todos os caractéres da string, ou seja, ele irá percorrer todos os caractéres até o fim
-		fmt.Printf("%c\n", letra) // Aqui estou utilizando o Printf que imprime o caractere como texto (e não o código Unicode)
+		fmt.Printf("%c\n", letra) // Aqui estou utilizando o Printf % que imprime o caractere como texto (e não o código Unicode)
 	}
 }
