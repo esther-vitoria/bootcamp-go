59d09f828b99905bb175b287c4710ee1f67d13b4
esther-vitoria <esther.vitoria21@outlook.com>
2025-05-30 16:43:37 -0300

    Exercicio 1 - Letras em uma palavra

---
+++ go-bases/Estrutura de controle/Exercicio 1 - Letras em uma palavra/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	palavra := "Borboleta" // Utilizando a declaração de variável resumida e atribuíndo o valor Borboleta
+
+	fmt.Println("Número de letras: ", len(palavra)) // Aqui estou imprimindo o quantas letras a palavra tem utilizando a função len(variavel)
+
+	for _, letra := range palavra { //Nesse for estou usando for seguido do underscore para percorrer todos os caractéres da string, ou seja, ele irá percorrer todos os caractéres até o fim
+		fmt.Printf("%c\n", letra) // Aqui estou utilizando o Printf que imprime o caractere como texto (e não o código Unicode)
+	}
+}
