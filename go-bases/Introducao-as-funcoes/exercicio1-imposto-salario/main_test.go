6a8282448619c86bdfd4520db1781ee4fff6405a
esther-vitoria <esther.vitoria21@outlook.com>
2025-06-02 19:49:20 -0300

    Novos testes unitários

--- go-bases/Introducao-as-funcoes/exercicio1-imposto-salario/main_test.go
+++ go-bases/Introducao-as-funcoes/exercicio1-imposto-salario/main_test.go
@@ -4,6 +4,15 @@ import (
 	"testing"
 )
 
+func TestCalcularImposto_AbaixoDe50000(t *testing.T) {
+	salario := 45000.0
+	esperado := salario * 0.10 // 10%
+	resultado := impostoSalario(salario)
+	if resultado != esperado {
+		t.Errorf("Esperado %.2f, mas obteve %.2f", esperado, resultado)
+	}
+}
+
 func TestCalcularImposto_AcimaDe50000(t *testing.T) {
 	salario := 149999.3
 	esperado := salario * 0.17 // 17%
